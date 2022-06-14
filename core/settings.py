6ff5f156c3c890d9dfb77924d6ebefa297327172
jatinsinghnp <js8971105@gmail.com>
2022-06-14 07:42:06 +0545

    all set to go33ll

--- core/settings.py
+++ core/settings.py
@@ -150,7 +150,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = "/media/"
 
 # Path where media is stored
-MEDIA_ROOT = BASE_DIR / "static/media/"
+MEDIA_ROOT = BASE_DIR / "static/my_media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
